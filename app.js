85a0572bef525201c5fb1f2f0a81d8522c88113b
KhaledDerbass <>
2021-05-05 11:11:34 +0000

    to push

---
+++ app.js
@@ -0,0 +1,13 @@
+alert('The Courses inserted below For higher education students ONLY!');
+
+var age=prompt('How old are you ?');
+
+if(age == 18 || age> 18)
+{
+   alert('you can search now !');
+
+}
+else
+{
+ alert('sorry! look at the Academy Section');
+}
