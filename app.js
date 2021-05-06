465a079542f8a0dfd1004e9e9b29e9f763e4b869
KhaledDerbass <>
2021-05-06 11:16:45 +0000

    to push website

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 alert('The Courses inserted below For higher education students ONLY!');
 
-var age=prompt('How old are you ?');
+var age = prompt('How old are you ?');
+var times;
 
-if(age == 18 || age> 18)
+if (age == 18 || age > 18) {
+  console.log('you can search now!');
+}
+else {
+while (!(age == 18 || age > 18))
 {
-   console.log('you can search now !');
+    age = prompt('How old are you ?');
+}
+console.log('Congrats, you can search now!');}
 
-}
-else
+
+
+times=prompt('Please insert # of Repetition time');
+for (var i=0;i<times;i++)
 {
- console.log('sorry! look at the Academy Section');
+document.write('<img src="courses.jpg" alt="Motivate study" width="15%">');
 }
+ 
+
+
+
