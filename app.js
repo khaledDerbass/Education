a38af776562da0ec542419bc4fb8f6edb80f710f
KhaledDerbass <>
2021-05-09 11:58:08 +0000

    to push lab

--- app.js
+++ app.js
@@ -2,30 +2,47 @@ document.getElementById("BtnSub").disabled = true;
 alert('The Courses inserted below For higher education students ONLY!');
 
 var age = prompt('How old are you ?');
-var times;
+var times=0;
 
-if (age == 18 || age > 18) {
-  console.log('you can search now!');
-document.getElementById("BtnSub").disabled = false;
+if (age == 18 || age > 18) 
+{
+  times=1;
+console.log('you can search now!');
+document.getElementById("BtnSub").disabled = false;}
 
-}
-else {
+else 
+{
+correctAge();
+if (times==1){
+ console.log('Congrats, you can search now!');
+ document.getElementById("BtnSub").disabled = false;}}
+
+document.write(RateMe());
+
+
+function correctAge()
+{
 while (!(age == 18 || age > 18))
-{
     age = prompt('How old are you ?');
-}
-console.log('Congrats, you can search now!');
-document.getElementById("BtnSub").disabled = false;
-}
 
-
-
-times=prompt('Please insert # of Repetition time');
-for (var i=0;i<times;i++)
-{
-document.write('<img src="images/courses.jpg" alt="Motivate study" width="15%">');
+times=1;
+return times;
 }
  
+
+function RateMe()
+{
+  var stars = prompt('Give us rating Please 1-5 (Stars)');
+  if (stars >= 5)
+  stars = 5;
+
+  var starResults='';
+
+  for (var i = 0; i < stars; i++) 
+  starResults += '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDvvIxXy4-x8TSIlWn-znOZirHvrsErjpxQw&usqp=CAU" width="50px">';
+
+  return starResults;
+}
 
 
 
