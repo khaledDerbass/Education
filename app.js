d8632d14f3b532f996a041efca1adb38b77e8ed3
KhaledDerbass <>
2021-05-09 04:00:00 +0000

    to push edited site

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+document.getElementById("BtnSub").disabled = true;
 alert('The Courses inserted below For higher education students ONLY!');
 
 var age = prompt('How old are you ?');
@@ -5,20 +6,24 @@ var times;
 
 if (age == 18 || age > 18) {
   console.log('you can search now!');
+document.getElementById("BtnSub").disabled = false;
+
 }
 else {
 while (!(age == 18 || age > 18))
 {
     age = prompt('How old are you ?');
 }
-console.log('Congrats, you can search now!');}
+console.log('Congrats, you can search now!');
+document.getElementById("BtnSub").disabled = false;
+}
 
 
 
 times=prompt('Please insert # of Repetition time');
 for (var i=0;i<times;i++)
 {
-document.write('<img src="courses.jpg" alt="Motivate study" width="15%">');
+document.write('<img src="images/courses.jpg" alt="Motivate study" width="15%">');
 }
  
 
